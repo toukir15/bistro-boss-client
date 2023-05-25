dcb444c3881e2a2c5898964d530441c5e5622eac
Tanvir <toukir.developer.bd@gmail.com>
2023-05-25 16:26:31 +0600

    Set Auth Provider: set auth provider in global context

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+import { app } from "../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <div className="max-w-5xl mx-auto">
-      <RouterProvider router={router} />
-    </div>
-  </HelmetProvider>
+  <AuthProvider>
+    <HelmetProvider>
+      <div className="max-w-5xl mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
+  </AuthProvider>
 );
